f58c7866cf732f0586a346ee1be36f2d938be955
teesid <teesid@instasim.com>
2018-01-26 15:47:18 +0700

    Added mintTimelocked() to the token contract.

--- contracts/DEVToken.sol
+++ contracts/DEVToken.sol
@@ -1,10 +1,25 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "zeppelin-solidity/contracts/token/ERC20/TokenTimelock.sol";
 
 contract DEVToken is PausableToken, MintableToken {
+  using SafeMath for uint256;
+
   string public name = "DEVToken";
   string public symbol = "DEV";
   uint8 public decimals = 18;
+  
+  /**
+   * @dev mint timelocked tokens
+   */
+  function mintTimelocked(address _to, uint256 _amount, uint256 _releaseTime)
+    onlyOwner canMint returns (TokenTimelock) {
+
+    TokenTimelock timelock = new TokenTimelock(this, _to, _releaseTime);
+    mint(timelock, _amount);
+
+    return timelock;
+  }
 }
