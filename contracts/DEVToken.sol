1442be22fc26089209757db355f3334a4c7db38a
teesid <teesid@instasim.com>
2018-02-15 13:00:43 +0700

    Tweaked the contract a bit so we can't finishMinting when paused and can't pause after minting is done.

--- contracts/DEVToken.sol
+++ contracts/DEVToken.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.18;
 
-import "zeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
+import "zeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
 import "zeppelin-solidity/contracts/token/ERC20/TokenTimelock.sol";
 
 contract DEVToken is PausableToken, MintableToken {
@@ -10,12 +10,26 @@ contract DEVToken is PausableToken, MintableToken {
   string public name = "DEVToken";
   string public symbol = "DEV";
   uint8 public decimals = 18;
+
+  /**
+   * @dev Override the pause function to ensure no one can pause after minting.
+   */
+  function pause() onlyOwner whenNotPaused canMint public {
+      super.pause();
+  }
+
+  /**
+   * @dev Make sure we cannot finish minting when paused, otherwise we are paused forever.
+   */
+  function finishMinting() onlyOwner whenNotPaused canMint public returns (bool) {
+      return super.finishMinting();
+  }
   
   /**
-   * @dev mint timelocked tokens
+   * @dev Mint timelocked tokens
    */
   function mintTimelocked(address _to, uint256 _amount, uint256 _releaseTime)
-    onlyOwner canMint returns (TokenTimelock) {
+    onlyOwner canMint public returns (TokenTimelock) {
 
     TokenTimelock timelock = new TokenTimelock(this, _to, _releaseTime);
     mint(timelock, _amount);
