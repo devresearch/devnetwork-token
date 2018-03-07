d903943a8a4f13cd3a5b1d1fd912e861bb08c1c1
Parinthorn Panya <maixezer@gmail.com>
2018-03-07 16:30:19 +0700

    Override transfer and transferForm, and mint method

--- contracts/DEVToken.sol
+++ contracts/DEVToken.sol
@@ -9,7 +9,20 @@ contract DEVToken is PausableToken, MintableToken {
 
   string public name = "DEVToken";
   string public symbol = "DEV";
-  uint8 public decimals = 18;
+  uint8  public decimals = 18;
+
+  bool   public transferEnabled = false; // indicates that tokens can transfer or not
+
+  // Modifiers
+  modifier onlyWhenTransferEnabled() {
+      require(transferEnabled);
+      _;
+  }
+
+  modifier validDestination(address _to) {
+      require(_to != address(owner));
+      _;
+  }
 
   /**
    * @dev Override the pause function to ensure no one can pause after minting.
@@ -26,14 +39,50 @@ contract DEVToken is PausableToken, MintableToken {
   }
   
   /**
-   * @dev Mint timelocked tokens
+   * @dev Mint tokens
    */
-  function mintTimelocked(address _to, uint256 _amount, uint256 _releaseTime)
-    onlyOwner canMint public returns (TokenTimelock) {
-
-    TokenTimelock timelock = new TokenTimelock(this, _to, _releaseTime);
-    mint(timelock, _amount);
-
-    return timelock;
+  function mintToken(address[] _to, uint256[] _amount)
+    onlyOwner canMint public returns (bool) 
+  {
+      for (uint i = 0; i < _to.length; i++) {
+        mint(_to[i], _amount[i]);
+      }
+      return true;
   }
+
+  /**
+   * @dev Override mint, cannot mint token for the owner of contract
+   */
+  function mint(address _to, uint256 _amount) 
+    onlyOwner canMint validDestination(_to) public returns (bool) 
+  {
+    return super.mint(_to, _amount);
+  }
+
+  /**
+   * @dev enable transfer, after it is not possible to disable
+   */
+  function enableTransfer() external onlyOwner {
+      transferEnabled = true;
+  }
+
+  /**
+   * @dev Overrides ERC20 transfer function with modifier that prevents the
+   * ability to transfer tokens until after transfers have been enabled.
+   */
+  function transfer(address _to, uint256 _value) 
+    public onlyWhenTransferEnabled validDestination(_to) returns (bool) 
+  {
+      return super.transfer(_to, _value);
+  }
+
+  /**
+   * @dev Overrides ERC20 transfer function with modifier that prevents the
+   * ability to transfer tokens until after transfers have been enabled.
+   */
+  function transferFrom(address _from, address _to, uint256 _value) 
+    public onlyWhenTransferEnabled validDestination(_to) returns (bool) 
+  {
+      return super.transferFrom(_from, _to, _value);
+  } 
 }
