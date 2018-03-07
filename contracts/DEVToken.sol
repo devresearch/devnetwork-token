6edf2b48d363b0c70be679f6925fc96623a86c19
Parinthorn Panya <maixezer@gmail.com>
2018-03-07 18:58:28 +0700

    remove import TokenTimeLock

--- contracts/DEVToken.sol
+++ contracts/DEVToken.sol
@@ -2,7 +2,6 @@ pragma solidity ^0.4.18;
 
 import "zeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "zeppelin-solidity/contracts/token/ERC20/PausableToken.sol";
-import "zeppelin-solidity/contracts/token/ERC20/TokenTimelock.sol";
 
 contract DEVToken is PausableToken, MintableToken {
   using SafeMath for uint256;
