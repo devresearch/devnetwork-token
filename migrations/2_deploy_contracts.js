22b10112a032f7b7d63f7d7cc99282359dd10b17
Parinthorn Panya <maixezer@gmail.com>
2018-03-08 18:11:49 +0700

    Modify Migration to receieve parameter and specfiy the network

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,15 @@
 const DEVToken = artifacts.require("./DEVToken.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(DEVToken);
+module.exports = function (deployer, network, accounts) {
+  const now = new Date()
+  const nowTimeUnix = Math.floor(now.getTime() / 1000)
+  const oneYearLaterTimeUnix = Math.floor(
+    new Date(new Date().setFullYear(now.getFullYear() + 1)).getTime() / 1000
+  )
+
+  if (network == 'live') {
+    deployer.deploy(DEVToken, process.env.FOUNDATION_ADDRESS, nowTimeUnix, oneYearLaterTimeUnix);
+  } else {
+    deployer.deploy(DEVToken, accounts[1], nowTimeUnix, oneYearLaterTimeUnix);
+  }
 };
