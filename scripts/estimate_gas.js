4ec04d61ec28a6852cffa930be817e0d8440eb43
Parinthorn Panya <maixezer@gmail.com>
2018-03-09 16:30:35 +0700

    Modified estimate gas for transfer and spreadToken

--- scripts/estimate_gas.js
+++ scripts/estimate_gas.js
@@ -32,7 +32,8 @@ web3.eth.getGasPrice()
           ...opts,
           owner: accounts[0],
           foundation: accounts[1],
-          restContributor: accounts.slice(2)
+          bounty: accounts[2],
+          restContributor: accounts.slice(3)
         }
       })
 
@@ -41,7 +42,7 @@ web3.eth.getGasPrice()
     // Estimate gas of contract
     return contract.deploy({
       data: compiledContract.bytecode,
-      arguments: [opts.foundation, nowTimeUnix, oneYearLaterTimeUnix]
+      arguments: [opts.foundation, opts.bounty, nowTimeUnix, oneYearLaterTimeUnix]
     })
       .estimateGas()
       .then((estimatedGas) => {
@@ -55,7 +56,7 @@ web3.eth.getGasPrice()
   .then((opts) => {
     return contract.deploy({
       data: compiledContract.bytecode,
-      arguments: [opts.foundation, nowTimeUnix, oneYearLaterTimeUnix]
+      arguments: [opts.foundation, opts.bounty, nowTimeUnix, oneYearLaterTimeUnix]
     }).send({
       from: opts.owner,
       gasPrice: opts.averageGasPrice,
@@ -74,7 +75,7 @@ web3.eth.getGasPrice()
     // address _to come from testrpc
     contract.options.address = opts.address
 
-    contract.methods['spreadToken(address,uint256,uint8)'](opts.restContributor[0], web3.utils.toBN('1e21'), 1)
+    contract.methods['spreadToken(address,uint256)'](opts.restContributor[0], web3.utils.toBN('1e21'))
       .estimateGas({ from: opts.owner })
       .then((gasAmount) => {
         console.log(`Estimated gas for spreadToken method: ${gasAmount}`)
@@ -84,8 +85,8 @@ web3.eth.getGasPrice()
   })
   .then((opts) => {
     // Maxmimum length than can handle array of address and amount is approximately 180
-    contract.methods['spreadTokenAddresses(address[],uint256[],uint8)']
-      (opts.restContributor, Array.from(new Array(opts.restContributor.length), (val, index) => web3.utils.toBN('1e22')), 1)
+    contract.methods['spreadTokenAddresses(address[],uint256[])']
+      (opts.restContributor, Array.from(new Array(opts.restContributor.length), (val, index) => web3.utils.toBN('1e22')))
       .estimateGas({ from: opts.owner })
       .then((gasAmount) => {
         console.log(`Estimated gas for spreadTokenAddresses method: ${gasAmount}`)
