12f1d9fa1858cc25c2fb3e1996814b5be7fb6066
Parinthorn Panya <maixezer@gmail.com>
2018-03-12 13:04:41 +0700

    Change migration scripts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,20 @@
 const DEVToken = artifacts.require("./DEVToken.sol");
 const TokenTimelock = artifacts.require("./TokenTimelock.sol");
 
-module.exports = function (deployer, network, accounts) {
+module.exports = function (deployer, network) {
   const now = new Date()
   const nowTimeUnix = Math.floor(now.getTime() / 1000)
   const oneYearLaterTimeUnix = Math.floor(
     new Date(new Date().setFullYear(now.getFullYear() + 1)).getTime() / 1000
   )
-  
+
+  // const thirtyMinutesLaterTimeUnix = Math.floor(
+  //   new Date(new Date().setMinutes(now.getMinutes() + 30)).getTime() / 1000
+  // )
+
+  console.log(`Deploy time is ${nowTimeUnix}`)
+  console.log(`Release time for tokenTimelock is ${thirtyMinutesLaterTimeUnix}`)
+
   if (network == 'test') {
     deployer.deploy(DEVToken);
   } else {
@@ -15,7 +22,7 @@ module.exports = function (deployer, network, accounts) {
       return deployer.deploy(
         TokenTimelock,
         DEVToken.address,
-        network == 'live' || network == 'ropsten' ? process.env.FOUNDATION : accounts[1],
+        process.env.FOUNDATION,
         oneYearLaterTimeUnix
       );
     });
